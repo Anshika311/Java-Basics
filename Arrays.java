8df946dd3dfc0bffd52ea9a7b79c09a82520f304
Anshika <116749450+Anshika311@users.noreply.github.com>
2023-05-31 00:10:40 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+package com.DIVA;
+
+public class Arrays {
+	public static void main(String args[]) {
+//		int marks[]= new int[3];
+		int marks[]= {89,79,99};
+//		marks[0]=89;
+//		marks[1]=79;
+//		marks[2]=96;
+//		System.out.println(marks[0]);
+//		System.out.println(marks[1]);
+//		System.out.println(marks[2]);
+		for(int i=0;i<3;i++)
+			System.out.println(marks[i]);
+	}
+
+}
