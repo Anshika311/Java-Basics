f8cf93af67b0c4ab6d6b4f98a5b6a7aaab78eb6b
Anshika <116749450+Anshika311@users.noreply.github.com>
2023-05-31 00:08:54 +0530

    Add files via upload

---
+++ ValueOf_xRaise_n.java
@@ -0,0 +1,23 @@
+package com.DIVA;
+
+import java.util.Scanner;
+
+public class ValueOf_xRaise_n {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x =sc.nextInt();
+		int n =sc.nextInt();
+		
+		
+		double value = Math.pow(x, n);
+		{
+			System.out.println(value);
+		}
+		
+		
+		
+
+	}
+
+}
