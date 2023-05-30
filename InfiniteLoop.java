bdc5db315325de994de06ff8b6af5c5e01cba22a
Anshika <116749450+Anshika311@users.noreply.github.com>
2023-05-31 00:08:02 +0530

    Add files via upload

---
+++ InfiniteLoop.java
@@ -0,0 +1,17 @@
+package com.DIVA;
+
+import java.util.Scanner;
+
+public class InfiniteLoop {
+
+	public static void main(String[] args) {
+		int x=100;
+		do { 
+			System.out.println("Hello");
+			
+		}
+		while(x>10);
+
+	}
+
+}
