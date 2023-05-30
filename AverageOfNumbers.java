caa46a18c0912ee45bc593ffcf4f25f4335d9e0a
Anshika <116749450+Anshika311@users.noreply.github.com>
2023-05-31 00:02:40 +0530

    Add files via upload

---
+++ AverageOfNumbers.java
@@ -0,0 +1,26 @@
+package com.DIVA;
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+//	public static double calculateAverage(double a, double b,double c) {
+//	double Average= (a+b+c)/3;
+	
+//	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	
+		double a=sc.nextInt();
+		double b=sc.nextInt();
+		double c=sc.nextInt();
+		double average =(a+b+c)/3;
+		{
+		System.out.println(average);
+		}
+	
+		
+
+	}
+
+}
