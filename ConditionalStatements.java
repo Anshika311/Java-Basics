b7d99ff339b85d64fce193012168cf99d1ef852f
Anshika <116749450+Anshika311@users.noreply.github.com>
2023-05-30 23:47:50 +0530

    Add files via upload

---
+++ ConditionalStatements.java
@@ -0,0 +1,45 @@
+package com.DIVA;
+
+import java.util.*;
+
+public class ConditionalStatements {
+	public static void main(String[]args) {
+		
+		//1.Adult or not
+		/*System.out.println("Enter age");
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		
+		if(age > 18) {
+			System.out.println("Adult");
+		}else {
+			System.out.println("Not Adult");*/
+		
+		//2.Even or Odd %=modulo remainder
+		/*System.out.println("Enter Number");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if(num%2==0) {
+			System.out.println("Even");
+			
+		}else {
+			System.out.println("Odd");*/
+		
+		//3.two numbers equal greater or lesser
+		/*System.out.println("Enter number");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a==b) {
+			System.out.println("Equal");
+		}
+		else if(a>b) {
+			System.out.println( a + " is greater");
+			
+		}else {
+			System.out.println( a + " is lesser");*/
+		}
+		
+	}
+
+
