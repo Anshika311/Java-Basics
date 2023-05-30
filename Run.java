9af6e0e46dbcdee6af8eecb3fa47e234b49eac60
Anshika <116749450+Anshika311@users.noreply.github.com>
2023-05-30 23:48:58 +0530

    Add files via upload

---
+++ Run.java
@@ -0,0 +1,17 @@
+package com.DIVA;
+
+public class Run {
+
+	public static void main(String[] args) {
+		/*infinite for loop happen when there is without
+		 * any exit condition. Also without initializing statement, 
+		 * condition or increment/decrement operation.
+		 * Should avoid infinite loops and computers memory becomes full */
+		for(int i=1; ; i++){
+		//for( ; ;) {
+			System.out.println("Apna College");
+		}
+
+	}
+
+}
