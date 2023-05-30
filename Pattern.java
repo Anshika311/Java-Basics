dddab3d6a57d82c18eccfea8969cef427b11e7d4
Anshika <116749450+Anshika311@users.noreply.github.com>
2023-05-30 23:57:00 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,55 @@
+package com.DIVA;
+
+import java.util.Scanner;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		//SOLID RECTANGLE
+		Scanner sc =new Scanner(System.in);
+		int n= sc.nextInt();
+		int m= sc.nextInt();
+		//outer loop
+		for(int i =1; i<=n;i++) {//i=row
+			//inner loop
+			for(int j=1; j<=m;j++) {//j=column
+				System.out.print("*");
+			}
+			System.out.println();
+		
+		
+		
+		
+		//HOLLOW RECTANGLE
+		/*Scanner sc =new Scanner(System.in);
+		int n= sc.nextInt();
+		int m= sc.nextInt();
+		//outer loop
+		for(int i =1; i<=n;i++) {//i=row
+			//inner loop
+			for(int j=1; j<=m;j++) {//j=column 
+			
+			if(i == 1 || j == 1 || i == n || j == m) {
+				
+			System.out.print("*");
+			
+		}else {
+			System.out.print(" ");
+		}
+			
+		}
+			System.out.println();*/
+		}
+		}
+	}
+
+
+
+		
+		
+	
+
+
+	
+
+
